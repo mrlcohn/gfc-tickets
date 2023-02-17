e5197a0448ec51e3d93e2e637d2fdf91e2b42d15
Leonard Cohn <leonardacohn@gmail.com>
2023-02-17 11:41:04 -0600

    Added simple splash page

--- gfc-tickets/src/App.js
+++ gfc-tickets/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import Welcome from "./Welcome";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Welcome />
     </div>
   );
 }
