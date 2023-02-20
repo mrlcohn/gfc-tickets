f772a62f46f1173b0d2fc263737eb4b4efab2e73
Leonard Cohn <leonardacohn@gmail.com>
2023-02-19 22:58:38 -0600

    Added very simple login database

---
+++ gfc-tickets/src/useFetch.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useFetch = url => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const abortCont = new AbortController();
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                }
+                else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
+            });
+        return () => abortCont.abort();
+    }, []);
+
+    return { data, isPending, error };
+}
