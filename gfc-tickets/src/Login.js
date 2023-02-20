8da618e0bc012d304aa75dd77c4b42a3a673109c
Leonard Cohn <leonardacohn@gmail.com>
2023-02-19 20:13:04 -0600

    Added simple login page

--- gfc-tickets/src/Login.js
+++ gfc-tickets/src/Login.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        navigate('/');
+    }
+
     return (
         <div className="login">
-            <p>Login</p>
+            <form className="entryForm" onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="item">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            required
+                            value={email}
+                            onChange = {e => setEmail(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="item">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            required
+                            value={password}
+                            onChange = {e => setPassword(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <button className="link">LOG IN</button>
+            </form>
         </div>
     );
 }
